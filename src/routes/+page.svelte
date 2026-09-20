9f18e32e023ab5ff414f36d288c52ce1ba2a11bd
Toy085 <brightabaka1@gmail.com>
2026-09-20 16:16:08 +0200

    Add basic homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Welcome to My website</h1>
+<script lang="ts">
+    console.log('Hello World!');
+</script>
+
+<body>
+    <h1>Welcome to My website</h1>
+    <button>Create</button>
+    <button>Host</button>
+    <button>Join</button>
+</body>
+
+<style>
+    body {
+        background-color: lightblue;
+        margin: 0;
+        padding: 20px;
+    }
+</style>
