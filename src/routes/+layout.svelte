6b12a0f737654295a0361cae5511585de9c835b4
Toy085 <brightabaka1@gmail.com>
2026-09-20 16:23:08 +0200

    Add tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
 
 	let { children } = $props();
 </script>
@@ -9,3 +10,7 @@
 </svelte:head>
 
 {@render children()}
+
+<style>
+	@reference "tailwindcss";
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,16 @@
     console.log('Hello World!');
 </script>
 
-<body>
-    <h1>Welcome to My website</h1>
-    <button>Create</button>
-    <button>Host</button>
-    <button>Join</button>
-</body>
+<h1 class="text-2xl font-bold mb-4">Welcome to My website</h1>
+
+<div class="flex gap-2">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create</button>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Host</button>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Join</button>
+</div>
 
 <style>
-    body {
+    :global(body) {
         background-color: lightblue;
         margin: 0;
         padding: 20px;
