2a7064f52a2bd665ba791b4ad5c9a47e53bd09b9
Toy085 <brightabaka1@gmail.com>
2026-09-21 10:11:42 +0200

    Update gameboard with titles for each column

--- src/lib/gameboard.svelte
+++ src/lib/gameboard.svelte
@@ -1,33 +1,66 @@
 <script lang="ts">
-    let props = $props();
+  let props = $props();
 
-    const cells = Array.from({ length: 25 }, (_, i) => i);
+  const cells = Array.from({ length: 25 }, (_, i) => i);
 </script>
 
-<div class="gameboard" id="gameBoard">
-    {#each cells as index}
-        <div class="cell bg-blue-500 hover:bg-blue-700" data-index={index}><p class="text-1xl font-normal text-black">{(index%5)*100+100}$</p></div>
+<div class="gameboard-container">
+  <div class="gameboard-titles">
+    {#each Array(5) as index}
+      <div class="cell-title bg-blue-700 hover:bg-blue-900" data-index={index}>
+        <h2 class="text-2xl font-bold text-white">Lorem</h2>
+      </div>
     {/each}
+  </div>
+
+  <div class="gameboard" id="gameBoard">
+    {#each cells as index}
+      <div class="cell bg-blue-500 hover:bg-blue-700" data-index={index}>
+        <p class="text-xl font-normal text-white">
+          ${(Math.floor(index / 5) + 1) * 100}
+        </p>
+      </div>
+    {/each}
+  </div>
 </div>
 
 <style>
-    .gameboard {
-        display: grid;
-        grid-template-columns: repeat(5, 250px);
-        grid-template-rows: repeat(5, 150px);
-        gap: 10px;
-        justify-content: center;
-        align-items: center;
-    }
+  .gameboard-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
 
-    .cell {
-        width: 250px;
-        height: 150px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 2rem;
-        cursor: pointer;
-        border-radius: var(--radius-lg);
-    }
+  .gameboard-titles,
+  .gameboard {
+    display: grid;
+    grid-template-columns: repeat(5, 200px);
+    gap: 10px;
+    justify-content: center;
+  }
+
+  .gameboard {
+    grid-template-rows: repeat(5, 100px);
+  }
+
+  .cell-title {
+    width: 200px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border-radius: var(--radius-lg);
+  }
+
+  .cell {
+    width: 200px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border-radius: var(--radius-lg);
+  }
 </style>
