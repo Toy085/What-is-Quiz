ddbe837f0fbfeb6692eb87143b00810d18bc99c3
Toy085 <brightabaka1@gmail.com>
2026-09-21 08:06:41 +0200

    Centered the buttons on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-    console.log('Hello World!');
 </script>
 
 <h1 class="text-2xl font-bold mb-4">Welcome to My website</h1>
 
-<div class="flex gap-2">
+<div class="flex gap-2 justify-center items-center">
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create</button>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Host</button>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Join</button>
