58a7ee7be0936b32ed552d35743882c80f5203ff
Toy085 <brightabaka1@gmail.com>
2026-09-23 08:01:51 +0200

    When you click on cells it grows

--- src/lib/gameboard.svelte
+++ src/lib/gameboard.svelte
@@ -4,6 +4,8 @@
   }
   let { creation = false } = $props();
 
+  let activeCell = $state<number | null>(null);
+
   const cells = Array.from({ length: 25 }, (_, i) => i);
 
   function editCell(index: number): void {
@@ -11,7 +13,8 @@
       console.log("Not in Creation mode");
       return;
     }
-    console.log("In Creation mode");
+    let cell = cells.at(index);
+    activeCell = activeCell === index ? null : index;
     return;
   }
 </script>
@@ -27,7 +30,7 @@
 
   <div class="gameboard" id="gameBoard">
     {#each cells as index}
-      <button type="button" class="cell bg-blue-500 hover:bg-blue-700" data-index={index} onclick={() => editCell(index)}>
+      <button type="button" class="cell bg-blue-500 hover:bg-blue-700" class:edit-cell={activeCell === index} data-index={index} onclick={() => editCell(index)}>
         <p class="text-xl font-normal text-white">
           ${(Math.floor(index / 5) + 1) * 100}
         </p>
@@ -74,5 +77,11 @@
     align-items: center;
     cursor: pointer;
     border-radius: var(--radius-lg);
+  }
+  .edit-cell {
+    height: 65vh;
+    width: 65vw;
+    z-index: 1;
+    transition: all 500ms;
   }
 </style>
